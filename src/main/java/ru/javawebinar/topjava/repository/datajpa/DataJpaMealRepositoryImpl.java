d4e31e0fbdbdd3abf4e9a10fea314119997c28dd
Валерий <valeriinalimov@mail.ru>
2017-01-17 19:03:57 +0700

    HW5 1-implements DataJpaMealRepositoryImpl

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -6,7 +6,7 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Collection;
 
 /**
  * GKislin
@@ -18,28 +18,35 @@ public class DataJpaMealRepositoryImpl implements MealRepository {
     @Autowired
     private CrudMealRepository crudRepository;
 
+    @Autowired
+    private CrudUserRepository crudUserRepository;
+
     @Override
-    public Meal save(Meal Meal, int userId) {
-        return null;
+    public Meal save(Meal meal, int userId) {
+        meal.setUser(crudUserRepository.getOne(userId));
+        if (!meal.isNew() && get(meal.getId(), userId) == null){
+            return null;
+        }
+        return crudRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudRepository.get(id, userId);
     }
 
     @Override
-    public List<Meal> getAll(int userId) {
-        return null;
+    public Collection<Meal> getAll(int userId) {
+        return crudRepository.getAll(userId);
     }
 
     @Override
-    public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+    public Collection<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
+        return crudRepository.getBetween(startDate, endDate, userId);
     }
 }
