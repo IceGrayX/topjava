b2df778ecfc5b3a8067ad070292a4ecef67013c4
Валерий <valeriinalimov@mail.ru>
2017-01-19 14:02:53 +0700

    HW5 Optional

--- src/test/java/ru/javawebinar/topjava/service/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/DataJpaUserServiceTest.java
@@ -1,11 +1,31 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import static ru.javawebinar.topjava.UserTestData.MATCHER;
+import static ru.javawebinar.topjava.UserTestData.USER;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 /**
  * Created by Валерий on 18.01.2017.
  */
 @ActiveProfiles(Profiles.DATAJPA)
 public class DataJpaUserServiceTest extends AbstractUserServiceTest {
+    @Test
+    public void testGetWithMeals() throws Exception{
+        User user = service.getWithMeals(USER_ID);
+        MATCHER.assertEquals(USER, user);
+        MealTestData.MATCHER.assertCollectionEquals(MealTestData.MEALS, user.getMeals());
+    }
+
+    @Test
+    public void testGetWithMealsNotFound() throws Exception{
+        thrown.expect(NotFoundException.class);
+        service.getWithMeals(1);
+    }
 }
